e15021b47ac1facb3872c2ae9a6892539c430f83
Kirill <kirill18pobedil19@gmail.com>
2023-07-25 19:51:55 +0300

    final commit d3

--- d3.py
+++ d3.py
@@ -1,18 +1,19 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-math_action = input("Enter math action: + - * / ")
+#1. Користувач вводить із клавіатури номер дня тижня (1-7). Необхідно вивести на екран назви дня тижня. Наприклад, якщо 1, на екрані напис понеділок, 2 — вівторок тощо.
+n = int(input("Select the number of the day of the week: "))
+if n == 1:
+     print ("Monday")
+if n == 2:
+     print("Tuesday")
+if n == 3:
+     print("Wednesday")
+if n == 4:
+     print("Thursday")
+if n == 5:
+     print("Friday")
+if n == 6:
+     print("Saturday")
+if n == 7:
+      print("Sunday")
+#2.Користувач вводить два числа.Визначити, чи рівні ці числа, і, якщо ні, вивести їх на екран у порядку зростання
 
-match math_action:
-    case "+":
-        print(f"{num1} {math_action} {num2} = {num1 + num2}")
-    case "-":
-        print(f"{num1} {math_action} {num2} = {num1 - num2}")
-    case "*":
-        print(f"{num1} {math_action} {num2} = {num1 * num2}")
-    case "/":
-        if num2 != 0:
-            print(f"{num1} {math_action} {num2} = {num1 / num2}")
-        else:
-            print("Division by zero!")
-    case _:
-        print("Incorrect math action!")
+
