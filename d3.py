401e7a23827d73e79762237dadc4ac6ebfc63568
Kirill <kirill18pobedil19@gmail.com>
2023-07-25 23:00:29 +0300

    commit n2 d3

--- d3.py
+++ d3.py
@@ -1,22 +1,17 @@
-#1. Користувач вводить із клавіатури номер дня тижня (1-7). Необхідно вивести на екран назви дня тижня. Наприклад, якщо 1, на екрані напис понеділок, 2 — вівторок тощо.
+#2. Користувач вводить два числа. Визначити, чи рівні ці числа, і, якщо ні, вивести їх на екран у порядку зростання
 try:
-   n = int(input("Select the number of the day of the week: "))
-   if n == 1:
-     print ("Monday")
-   if n == 2:
-     print("Tuesday")
-   if n == 3:
-     print("Wednesday")
-   if n == 4:
-     print("Thursday")
-   if n == 5:
-     print("Friday")
-   if n == 6:
-     print("Saturday")
-   if n == 7:
-       print("Sunday")
+  j = int(input("Enter first number: "))
+  k = int(input("Enter second number: "))
+  if j == k:
+    print("The numbers are equal")
+  else:
+    print("Numbers are not equal")
+    if j < k:
+     for i in range (j, k +1):
+      print(i)
 except ValueError as error:
         print("Enter only integer numbers please!")
         print(f"ValueError: {error}")
 except Exception as error:
  print(f"Exception occurred: {error}")
+
